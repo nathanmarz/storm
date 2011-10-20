c50f960974a3d78b137f79c634d544872a8b4ea5
Nathan Marz <nathan@twitter.com>
2011-10-19 21:14:32 -0700

    fix storm kill command

--- src/clj/backtype/storm/command/kill_topology.clj
+++ src/clj/backtype/storm/command/kill_topology.clj
@@ -8,14 +8,14 @@
 (defn -main [& args]
   (with-command-line args
     "Kill a topology"
-    [[wait? w? "Override the amount of time to wait after deactivating before killing" nil]
+    [[wait w "Override the amount of time to wait after deactivating before killing" nil]
      posargs]    
     (let [conf (read-storm-config)
           host (conf NIMBUS-HOST)
           port (conf NIMBUS-THRIFT-PORT)
           name (first posargs)
           opts (KillOptions.)]
-      (if wait? (.set_wait_secs opts (Integer/parseInt wait?)))
+      (if wait (.set_wait_secs opts (Integer/parseInt wait)))
       (with-nimbus-connection [nimbus host port]
         (.killTopologyWithOpts nimbus name opts)
         (log-message "Killed topology: " name)

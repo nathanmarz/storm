a2a004abe1af96c056788fef80ec996f7bed457c
Nathan Marz <nathan@twitter.com>
2011-10-22 01:05:25 -0700

    auto-reconnect drpc client

--- src/jvm/backtype/storm/utils/DRPCClient.java
+++ src/jvm/backtype/storm/utils/DRPCClient.java
@@ -9,23 +9,26 @@ import org.apache.thrift7.transport.TFramedTransport;
 import org.apache.thrift7.transport.TSocket;
 import org.apache.thrift7.transport.TTransport;
 
-//TODO: needs to auto-reconnect
 public class DRPCClient implements DistributedRPC.Iface {
     private TTransport conn;
     private DistributedRPC.Client client;
     private String host;
-    private int port;
+    private int port;    
 
     public DRPCClient(String host, int port) {
         try {
             this.host = host;
             this.port = port;
-            conn = new TFramedTransport(new TSocket(host, port));
-            client = new DistributedRPC.Client(new TBinaryProtocol(conn));
-            conn.open();
+            connect();
         } catch(TException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    private void connect() throws TException {
+        conn = new TFramedTransport(new TSocket(host, port));
+        client = new DistributedRPC.Client(new TBinaryProtocol(conn));
+        conn.open();
     }
     
     public String getHost() {
@@ -34,26 +37,49 @@ public class DRPCClient implements DistributedRPC.Iface {
     
     public int getPort() {
         return port;
-    }
-
+    }   
+    
     public String execute(String func, String args) throws TException, DRPCExecutionException {
-        return client.execute(func, args);
+        try {
+            if(client==null) connect();
+            return client.execute(func, args);
+        } catch(TException e) {
+            client = null;
+            throw e;
+        } catch(DRPCExecutionException e) {
+            client = null;
+            throw e;
+        }
     }
 
     public void result(String id, String result) throws TException {
-        client.result(id, result);
+        try {
+            if(client==null) connect();
+            client.result(id, result);
+        } catch(TException e) {
+            client = null;
+            throw e;
+        }
     }
 
     public DRPCRequest fetchRequest(String func) throws TException {
-        return client.fetchRequest(func);
+        try {
+            if(client==null) connect();
+            return client.fetchRequest(func);
+        } catch(TException e) {
+            client = null;
+            throw e;
+        }
     }    
 
     public void failRequest(String id) throws TException {
-        client.failRequest(id);
-    }        
-    
-    public DistributedRPC.Client getClient() {
-        return client;
+        try {
+            if(client==null) connect();
+            client.failRequest(id);
+        } catch(TException e) {
+            client = null;
+            throw e;
+        }
     }
 
     public void close() {

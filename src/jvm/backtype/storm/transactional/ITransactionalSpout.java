80adc6a73e981ee671fb35bbba2d8734bb914da0
Nathan Marz <nathan@twitter.com>
2011-12-23 03:52:13 -0800

    notes on transactionalspout implementation

--- src/jvm/backtype/storm/transactional/ITransactionalSpout.java
+++ src/jvm/backtype/storm/transactional/ITransactionalSpout.java
@@ -2,6 +2,10 @@ package backtype.storm.transactional;
 
 import java.util.Map;
 
+// TODO: acts identically to a bolt EXCEPT for the coordinator task which:
+//  -- keeps track of open transactions
+//  -- persists the state of committed transactions
+//  -- sends out batch and commit tuples
 public interface ITransactionalSpout {
     void open(Map conf);
     void close();

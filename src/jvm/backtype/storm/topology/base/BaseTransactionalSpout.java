60613149b802e88064383461319ef708aa69e69d
Nathan Marz <nathan@twitter.com>
2012-01-16 02:23:42 -0800

    fix BaseTransactionalSpout

--- src/jvm/backtype/storm/topology/base/BaseTransactionalSpout.java
+++ src/jvm/backtype/storm/topology/base/BaseTransactionalSpout.java
@@ -1,10 +1,6 @@
 package backtype.storm.topology.base;
 
-import backtype.storm.task.TopologyContext;
-import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.transactional.ITransactionalSpout;
-import backtype.storm.transactional.TransactionAttempt;
-import backtype.storm.transactional.TransactionalOutputCollector;
 import java.util.Map;
 
 public abstract class BaseTransactionalSpout implements ITransactionalSpout {
@@ -12,14 +8,6 @@ public abstract class BaseTransactionalSpout implements ITransactionalSpout {
     
     public BaseTransactionalSpout(String id) {
         _id = id;
-    }
-    
-    @Override
-    public void open(Map conf, TopologyContext context) {
-    }
-
-    @Override
-    public void close() {        
     }
 
     @Override

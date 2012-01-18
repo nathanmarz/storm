e12b26878d95ab02325baed7902f8c55cfb245ff
Nathan Marz <nathan@twitter.com>
2012-01-18 03:28:37 -0800

    exclude zookeeper client from storm-kafka since it's already available from storm

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :dependencies [[storm/kafka "0.6.0"]]
+  :dependencies [[storm/kafka "0.6.0"
+                   :exclusions [org.apache.zookeeper/zookeeper]]]
   :dev-dependencies [[storm "0.7.0-SNAPSHOT"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )

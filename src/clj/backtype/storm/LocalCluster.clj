d38b378ab58c7904af31700f2adbd8a2ce1fa8d1
James Xu <xumingming64398966@yahoo.com.cn>
2012-02-16 21:17:03 +0800

    add validation for LocalCluster and fix a NullPointerException in StormSubmitter#validateStormConf

--- src/clj/backtype/storm/LocalCluster.clj
+++ src/clj/backtype/storm/LocalCluster.clj
@@ -1,5 +1,6 @@
 (ns backtype.storm.LocalCluster
   (:use [backtype.storm testing])
+  (:import [backtype.storm StormSubmitter])
   (:gen-class
    :init init
    :implements [backtype.storm.ILocalCluster]
@@ -12,6 +13,8 @@
     ))
 
 (defn -submitTopology [this name conf topology]
+  ; validate the config first
+  (StormSubmitter/validateStormConf conf)
   (submit-local-topology (:nimbus (. this state))
                       name
                       conf

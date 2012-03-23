635b483f48e2a6ff4cfdb68a10ced817cf8bdda1
Nathan Marz <nathan@twitter.com>
2012-03-22 21:53:04 -0700

    add rest of nimbus methods to localcluster

--- src/clj/backtype/storm/LocalCluster.clj
+++ src/clj/backtype/storm/LocalCluster.clj
@@ -18,25 +18,31 @@
                       topology))
 
 (defn -shutdown [this]
-  (kill-local-storm-cluster (. this state))
-  )
+  (kill-local-storm-cluster (. this state)))
 
 (defn -killTopology [this name]
-  (.killTopology (:nimbus (. this state)) name)
-  )
+  (.killTopology (:nimbus (. this state)) name))
 
 (defn -getTopologyConf [this id]
-  (.getTopologyConf (:nimbus (. this state)) id)
-  )
+  (.getTopologyConf (:nimbus (. this state)) id))
 
 (defn -getTopology [this id]
-  (.getTopology (:nimbus (. this state)) id)
-  )
+  (.getTopology (:nimbus (. this state)) id))
 
 (defn -getClusterInfo [this]
-  (.getClusterInfo (:nimbus (. this state)))
-  )
+  (.getClusterInfo (:nimbus (. this state))))
 
 (defn -getTopologyInfo [this id]
-  (.getTopologyInfo (:nimbus (. this state)) id)
-  )
+  (.getTopologyInfo (:nimbus (. this state)) id))
+
+(defn -killTopologyWithOpts [this name opts]
+  (.killTopologyWithOpts (:nimbus (. this state)) name opts))
+
+(defn -activate [this name]
+  (.activate (:nimbus (. this state)) name))
+
+(defn -deactivate [this name]
+  (.deactivate (:nimbus (. this state)) name))
+
+(defn -rebalance [this name opts]
+  (.rebalance (:nimbus (. this state)) name opts))
--- src/jvm/backtype/storm/ILocalCluster.java
+++ src/jvm/backtype/storm/ILocalCluster.java
@@ -3,7 +3,9 @@ package backtype.storm;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.ClusterSummary;
 import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.generated.KillOptions;
 import backtype.storm.generated.NotAliveException;
+import backtype.storm.generated.RebalanceOptions;
 import backtype.storm.generated.StormTopology;
 import backtype.storm.generated.TopologyInfo;
 
@@ -11,10 +13,12 @@ import java.util.Map;
 
 
 public interface ILocalCluster {
-    void submitTopology(String topologyName, Map conf, StormTopology topology)
-            throws AlreadyAliveException, InvalidTopologyException;
-    void killTopology(String topologyName)
-            throws NotAliveException;
+    void submitTopology(String topologyName, Map conf, StormTopology topology) throws AlreadyAliveException, InvalidTopologyException;
+    void killTopology(String topologyName) throws NotAliveException;
+    void killTopologyWithOpts(String name, KillOptions options) throws NotAliveException;
+    void activate(String topologyName) throws NotAliveException;
+    void deactivate(String topologyName) throws NotAliveException;
+    void rebalance(String name, RebalanceOptions options) throws NotAliveException;
     void shutdown();
     String getTopologyConf(String id);
     StormTopology getTopology(String id);

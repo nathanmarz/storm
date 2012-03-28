6cff057ea69f98094d7957777ef22e759db86b13
Nathan Marz <nathan@twitter.com>
2012-03-27 23:55:41 -0700

    added custom groupings to lineardrpc api

--- src/jvm/backtype/storm/drpc/LinearDRPCInputDeclarer.java
+++ src/jvm/backtype/storm/drpc/LinearDRPCInputDeclarer.java
@@ -1,5 +1,6 @@
 package backtype.storm.drpc;
 
+import backtype.storm.grouping.CustomStreamGrouping;
 import backtype.storm.topology.ComponentConfigurationDeclarer;
 import backtype.storm.tuple.Fields;
 
@@ -23,5 +24,9 @@ public interface LinearDRPCInputDeclarer extends ComponentConfigurationDeclarer<
     public LinearDRPCInputDeclarer allGrouping(String streamId);
 
     public LinearDRPCInputDeclarer directGrouping();
-    public LinearDRPCInputDeclarer directGrouping(String streamId);    
+    public LinearDRPCInputDeclarer directGrouping(String streamId);
+    
+    public LinearDRPCInputDeclarer customGrouping(CustomStreamGrouping grouping);
+    public LinearDRPCInputDeclarer customGrouping(String streamId, CustomStreamGrouping grouping);
+    
 }
--- src/jvm/backtype/storm/drpc/LinearDRPCTopologyBuilder.java
+++ src/jvm/backtype/storm/drpc/LinearDRPCTopologyBuilder.java
@@ -10,6 +10,7 @@ import backtype.storm.coordination.CoordinatedBolt.SourceArgs;
 import backtype.storm.coordination.IBatchBolt;
 import backtype.storm.generated.StormTopology;
 import backtype.storm.generated.StreamInfo;
+import backtype.storm.grouping.CustomStreamGrouping;
 import backtype.storm.topology.BaseConfigurationDeclarer;
 import backtype.storm.topology.BasicBoltExecutor;
 import backtype.storm.topology.BoltDeclarer;
@@ -325,6 +326,28 @@ public class LinearDRPCTopologyBuilder {
                 @Override
                 public void declare(String prevComponent, InputDeclarer declarer) {
                     declarer.directGrouping(prevComponent, streamId);
+                }                
+            });
+            return this;
+        }
+        
+        @Override
+        public LinearDRPCInputDeclarer customGrouping(final CustomStreamGrouping grouping) {
+            addDeclaration(new InputDeclaration() {
+                @Override
+                public void declare(String prevComponent, InputDeclarer declarer) {
+                    declarer.customGrouping(prevComponent, grouping);
+                }                
+            });
+            return this;
+        }
+
+        @Override
+        public LinearDRPCInputDeclarer customGrouping(final String streamId, final CustomStreamGrouping grouping) {
+            addDeclaration(new InputDeclaration() {
+                @Override
+                public void declare(String prevComponent, InputDeclarer declarer) {
+                    declarer.customGrouping(prevComponent, streamId, grouping);
                 }                
             });
             return this;

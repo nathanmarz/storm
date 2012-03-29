b76c41b58a1bce5abb59a8814f9ff11699420bb1
Thomas Jack <thomasjack@gmail.com>
2012-03-28 23:15:33 -0500

    shell protocol json compliance and spout implementation for ruby

--- src/multilang/rb/storm.rb
+++ src/multilang/rb/storm.rb
@@ -3,7 +3,14 @@ require "json"
 
 module Storm
   module Protocol
-    def read_string_message
+    class << self
+      attr_accessor :mode, :pending_taskids, :pending_commands
+    end
+
+    self.pending_taskids = []
+    self.pending_commands = []
+
+    def read_message
       msg = ""
       loop do
         line = STDIN.readline.chomp
@@ -11,45 +18,80 @@ module Storm
         msg << line
         msg << "\n"
       end
-      msg.chomp
+      JSON.parse msg.chomp
     end
 
-    def read_message
-      JSON.parse read_string_message
+    def read_task_ids
+      Storm::Protocol.pending_taskids.shift ||
+        begin
+          msg = read_message
+          until msg.is_a? Array
+            Storm::Protocol.pending_commands.push(msg)
+            msg = read_message
+          end
+          msg
+        end
     end
 
-    def send_to_parent(s)
-      puts s
+    def read_command
+      Storm::Protocol.pending_commands.shift ||
+        begin
+          msg = read_message
+          while msg.is_a? Array
+            Storm::Protocol.pending_taskids.push(msg)
+            msg = read_message
+          end
+          msg
+        end
+    end
+
+    def send_msg_to_parent(msg)
+      puts msg.to_json
       puts "end"
       STDOUT.flush
     end
 
     def sync
-      puts "sync"
-      STDOUT.flush
+      send_msg_to_parent({'command' => 'sync'})
     end
 
     def send_pid(heartbeat_dir)
       pid = Process.pid
-      puts pid
-      STDOUT.flush
+      send_msg_to_parent({'pid' => pid})
       File.open("#{heartbeat_dir}/#{pid}", "w").close
     end
 
-    def send_msg_to_parent(hash)
-      send_to_parent hash.to_json
-    end
-
-    def emit_tuple(tup, stream = nil, anchors = [], direct = nil)
+    def emit_bolt(tup, args = {})
+      stream = args[:stream]
+      anchors = args[:anchors] || args[:anchor] || []
+      anchors = [anchors] unless anchors.is_a? Enumerable
+      direct = args[:direct_task]
       m = {:command => :emit, :anchors => anchors.map(&:id), :tuple => tup}
       m[:stream] = stream if stream
       m[:task] = direct if direct
       send_msg_to_parent m
+      read_task_ids unless direct
     end
 
-    def emit(tup, stream = nil, anchors = [], direct = nil)
-      emit_tuple tup, stream, anchors, direct
-      read_message
+    def emit_spout(tup, args = {})
+      stream = args[:stream]
+      id = args[:id]
+      direct = args[:direct_task]
+      m = {:command => :emit, :tuple => tup}
+      m[:id] = id if id
+      m[:stream] = stream if stream
+      m[:task] = direct if direct
+      send_msg_to_parent m
+      read_task_ids unless direct
+    end
+
+    def emit(*args)
+      case Storm::Protocol.mode
+      when 'spout'
+        emit_spout(*args)
+      when 'bolt'
+        emit_bolt(*args)
+      end
     end
 
     def ack(tup)
@@ -64,8 +106,11 @@ module Storm
       send_msg_to_parent :command => :log, :msg => msg.to_s
     end
 
-    def read_env
-      [read_message, read_message]
+    def handshake
+      heartbeat_dir = read_message['pidDir']
+      send_pid heartbeat_dir
+      setup_info = read_message
+      [setup_info['conf'], setup_info['context']]
     end
   end
 
@@ -88,29 +133,54 @@ module Storm
   class Bolt
     include Storm::Protocol
 
-    def initialize(conf = nil, context = nil)
-      @conf = conf
-      @context = context
-    end
+    def prepare(conf, context); end
 
-    def process(tuple)
-    end
+    def process(tuple); end
 
     def run
-      heartbeat_dir = read_string_message
-      send_pid heartbeat_dir
-      @conf, @context = read_env
+      Storm::Protocol.mode = 'bolt'
+      prepare(*handshake)
       begin
         while true
-          process Tuple.from_hash(read_message)
-          sync
+          process Tuple.from_hash(read_command)
         end
       rescue Exception => e
-        log e
+        log 'Exception in bolt: ' + e.message + ' - ' + e.backtrace.join('\n')
       end
     end
   end
 
   class Spout
+    include Storm::Protocol
+
+    def open(conf, context); end
+
+    def nextTuple; end
+
+    def ack(id); end
+
+    def fail(id); end
+
+    def run
+      Storm::Protocol.mode = 'spout'
+      open(*handshake)
+
+      begin
+        while true
+          msg = read_command
+          case msg['command']
+          when 'next'
+            nextTuple
+          when 'ack'
+            ack(msg['id'])
+          when 'fail'
+            fail(msg['id'])
+          end
+          sync
+        end
+      rescue Exception => e
+        log 'Exception in spout: ' + e.message + ' - ' + e.backtrace.join('\n')
+      end
+    end
   end
 end

c1e5112d7459b890bc70816e3a26a98b37f4e72a
Nathan Marz <nathan@twitter.com>
2012-04-02 13:08:07 -0700

    storm 0.7.1 compatible

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject storm/storm-kafka "0.7.0-kafka7-SNAPSHOT"
+(defproject storm/storm-kafka "0.7.1-kafka7-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dependencies [[storm/kafka "0.7.0-incubating"
                    :exclusions [org.apache.zookeeper/zookeeper
                                 log4j/log4j]]]
-  :dev-dependencies [[storm "0.7.0"]
+  :dev-dependencies [[storm "0.7.1"]
                      [org.clojure/clojure "1.2.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )

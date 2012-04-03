0e0497ce0f4e7427dc84fb154080f9bb8b41b44d
Nathan Marz <nathan@twitter.com>
2012-04-03 15:00:48 -0700

    add comment to IBasicBolt

--- src/jvm/backtype/storm/topology/IBasicBolt.java
+++ src/jvm/backtype/storm/topology/IBasicBolt.java
@@ -6,6 +6,11 @@ import java.util.Map;
 
 public interface IBasicBolt extends IComponent {
     void prepare(Map stormConf, TopologyContext context);
+    /**
+     * Process the input tuple and optionally emit new tuples based on the input tuple.
+     * 
+     * All acking is managed for you. Throw a FailedException if you want to fail the tuple.
+     */
     void execute(Tuple input, BasicOutputCollector collector);
     void cleanup();
 }

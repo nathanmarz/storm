e258cef74bd9f828df2307acfb5dc7d99e3e75cb
Nathan Marz <nathan@twitter.com>
2012-05-04 10:23:05 -0400

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-kafka "0.7.1-snap"
+(defproject storm/storm-kafka "0.7.1-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}

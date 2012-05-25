24fbd922b60213f776c68b2402e88d22ff50d254
Nathan Marz <nathan@twitter.com>
2012-05-25 16:14:26 -0700

    upgrade to 0.7.2-rc1

--- project.clj
+++ project.clj
@@ -9,12 +9,12 @@
                  "twitter4j" "http://twitter4j.org/maven2"
                  }
 
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [
                  [org.twitter4j/twitter4j-core "2.2.6-SNAPSHOT"]
                  [org.twitter4j/twitter4j-stream "2.2.6-SNAPSHOT"]
                  ]
 
-  :dev-dependencies [[storm "0.7.1"]
+  :dev-dependencies [[storm "0.7.2-rc1"]
+                     [org.clojure/clojure "1.4.0"]
                      ])
 

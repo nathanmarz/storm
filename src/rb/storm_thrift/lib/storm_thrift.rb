ce3386bb754e8c357659f23b3edf148d67a5f836
Argyris Zymnis <argyris@twitter.com>
2012-05-25 11:00:32 -0700

    Rename imports

--- src/rb/storm_thrift/lib/storm_thrift.rb
+++ src/rb/storm_thrift/lib/storm_thrift.rb
@@ -5,8 +5,8 @@ require 'thrift'
 
 ## START THRIFT REQUIRE STATEMENTS ##
 require 'storm_thrift/thrift/nimbus'
-require 'storm_thrift/thrift/distributed_rpc'
-require 'storm_thrift/thrift/distributed_rpc_invocations'
+require 'storm_thrift/thrift/distributed_r_p_c'
+require 'storm_thrift/thrift/distributed_r_p_c_invocations'
 ## END THRIFT REQUIRE STATEMENTS ##
 
 require 'storm_thrift/version'

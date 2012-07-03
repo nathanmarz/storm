b7ba9d9455582b8ef180b96d7f543907b74b674f
Nathan Marz <nathan@twitter.com>
2012-07-02 18:42:24 -0700

    make brokerhosts serializable

--- src/jvm/storm/kafka/KafkaConfig.java
+++ src/jvm/storm/kafka/KafkaConfig.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KafkaConfig implements Serializable {
-    public static interface BrokerHosts {
+    public static interface BrokerHosts extends Serializable {
         
     }
     

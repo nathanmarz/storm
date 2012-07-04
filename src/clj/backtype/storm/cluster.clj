86904a66dc1e461c42f64e9d393347cb734046e4
Nathan Marz <nathan@twitter.com>
2012-07-04 12:52:31 -0700

    support zookeeper authentication for system zookeeper clients

--- src/clj/backtype/storm/cluster.clj
+++ src/clj/backtype/storm/cluster.clj
@@ -22,7 +22,7 @@
   )
 
 (defn mk-distributed-cluster-state [conf]
-  (let [zk (zk/mk-client conf (conf STORM-ZOOKEEPER-SERVERS) (conf STORM-ZOOKEEPER-PORT))]
+  (let [zk (zk/mk-client conf (conf STORM-ZOOKEEPER-SERVERS) (conf STORM-ZOOKEEPER-PORT) :auth-conf conf)]
     (zk/mkdirs zk (conf STORM-ZOOKEEPER-ROOT))
     (.close zk))
   (let [callbacks (atom {})
@@ -30,6 +30,7 @@
         zk (zk/mk-client conf
                          (conf STORM-ZOOKEEPER-SERVERS)
                          (conf STORM-ZOOKEEPER-PORT)
+                         :auth-conf conf
                          :root (conf STORM-ZOOKEEPER-ROOT)
                          :watcher (fn [state type path]
                                      (when @active

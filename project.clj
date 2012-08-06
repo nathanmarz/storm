7bfcb121ba2ef957ace4abc4fd552bd3e7ac63b0
Argyris Zymnis <argyris@twitter.com>
2012-08-06 13:49:05 -0700

    Move to scala 2.9.2

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject storm/storm-kafka "0.7.3-dynamic-SNAPSHOT"
+(defproject storm/storm-kafka "0.7.3-scala292-dynamic-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :repositories {"releases" "http://artifactory.local.twitter.com/libs-releases-local"
-                 "snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"
-                 "artifactory" "http://artifactory.local.twitter.com/repo"}
-  :dependencies [[storm/kafka "0.7.0-incubating"
+  ;;:repositories {"releases" "http://artifactory.local.twitter.com/libs-releases-local"
+  ;;               "snapshots" "http://artifactory.local.twitter.com/libs-snapshots-local"
+  ;;               "artifactory" "http://artifactory.local.twitter.com/repo"}
+  :dependencies [[com.twitter/kafka_2.9.2 "0.7.0"
                    :exclusions [org.apache.zookeeper/zookeeper
                                 log4j/log4j]]]
-  :dev-dependencies [[storm "0.7.2"]
+  :dev-dependencies [[storm "0.8.0-SNAPSHOT"]
                      [org.clojure/clojure "1.4.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )

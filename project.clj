4e7868ef2e7d22b3649953d9ae9dc86ae0799246
Nathan Marz <nathan@twitter.com>
2012-08-06 13:47:25 -0700

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-kafka "0.7.3-dynamic-storm8-SNAPSHOT"
+(defproject storm/storm-kafka "0.8.0-wip1"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
@@ -7,7 +7,7 @@
   :dependencies [[storm/kafka "0.7.0-incubating"
                    :exclusions [org.apache.zookeeper/zookeeper
                                 log4j/log4j]]]
-  :dev-dependencies [[storm "0.8.0-SNAPSHOT"]
+  :dev-dependencies [[storm "0.8.0"]
                      [org.clojure/clojure "1.4.0"]]
   :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
 )

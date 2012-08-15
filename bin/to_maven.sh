5b3bec2362ab1df8316a9bdd2b4a6b05b1ed1fe1
Jason Jackson <jason@cvk.ca>
2012-08-15 12:25:02 -0700

    remove log4j references. change logging policy, 1GB max logs.

--- bin/to_maven.sh
+++ bin/to_maven.sh
@@ -1,22 +1,36 @@
-#!/bin/bash
+#!/bin/bash 
+function quit {
+    exit 1
+}
+trap quit 1 2 3 15  #Ctrl+C exits.
 
 RELEASE=`head -1 project.clj | awk '{print $3}' | sed -e 's/\"//' | sed -e 's/\"//'`
 
 rm -rf classes
-rm *jar
-rm *xml
+rm -f *jar
+rm -f *xml
 lein jar
 lein pom
 scp storm*jar pom.xml clojars@clojars.org:
 
 rm *jar
 rm -rf classes
-rm conf/log4j.properties
+rm conf/logback.xml
 lein jar
+
+cp project.clj orig-project.clj
+sed -i '' -e 's/\[.*logback[^]]*\]//g' project.clj
+sed -i '' -e 's/\[.*log4j-over-slf4j[^]]*\]//g' project.clj
+
+lein pom
+mv orig-project.clj project.clj
+
 mv pom.xml old-pom.xml
 sed 's/artifactId\>storm/artifactId\>storm-lib/g' old-pom.xml > pom.xml
 mv storm-$RELEASE.jar storm-lib-$RELEASE.jar
 scp storm*jar pom.xml clojars@clojars.org:
 rm *xml
 rm *jar
-git checkout conf/log4j.properties
+git checkout conf/logback.xml
+
+

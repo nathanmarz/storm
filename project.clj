99c7bcf801abf38fcc1df629c2700dbad5e04e35
Jason Jackson <jason@cvk.ca>
2012-08-23 14:20:26 -0700

    removed SNAPSHOT from project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-kafka "0.7.4-dynamic-json-SNAPSHOT"
+(defproject storm/storm-kafka "0.7.5-dynamic-json"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}

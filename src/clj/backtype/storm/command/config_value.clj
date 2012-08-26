c67b3df4e6332d3aa2a67e243e2d5c6668ab284e
Junichiro Takagi <t.junichiro@gmail.com>
2012-08-26 15:41:14 +0900

    Refs #243, added -conffile and -confdir option.

--- src/clj/backtype/storm/command/config_value.clj
+++ src/clj/backtype/storm/command/config_value.clj
@@ -2,8 +2,13 @@
   (:use [backtype.storm config log])
   (:gen-class))
 
+(defn -main
+  ([^String name]
+    (let [conf (read-storm-config)]
+      (println "VALUE:" (conf name))
+      ))
+  ([^String name ^String file]
+    (let [conf (read-storm-config file)]
+      (println "VALUE:" (conf name))
+      )))
 
-(defn -main [^String name]
-  (let [conf (read-storm-config)]
-    (println "VALUE:" (conf name))
-    ))

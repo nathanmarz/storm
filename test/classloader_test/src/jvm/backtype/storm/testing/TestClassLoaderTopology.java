0307e0e29b0e3ede8c4123d2f48c285a14ba6c78
James Xu <xumingming64398966@gmail.com>
2012-09-01 12:40:44 +0800

    add comments

--- test/classloader_test/src/jvm/backtype/storm/testing/TestClassLoaderTopology.java
+++ test/classloader_test/src/jvm/backtype/storm/testing/TestClassLoaderTopology.java
@@ -6,7 +6,9 @@ import backtype.storm.testing.Bar;
 public class TestClassLoaderTopology {
 	public static void main(String[] args) {
 		System.out.println("=======================================================================");
+		// Bar.foo will load the backtype.storm.testing.Foo from Storm core
 		Bar.foo();
+		// The following loads the backtype.storm.testing.Foo from classloader_test.
 		System.out.println("classloader_test::backtype.storm.testing.Foo loaded by " + Foo.class.getClassLoader());
 	}
 }

28be28005284cf9028cc107581a88ff286d00519
James Xu <xumingming64398966@gmail.com>
2012-09-02 00:57:59 +0800

    add cleanup command

--- test/classloader_test/run.sh
+++ test/classloader_test/run.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+touch /tmp/storm-classloader-test.txt
 lein jar
 ../../bin/storm run backtype.storm.testing.TestClassLoaderTopology *.jar
 cat /tmp/storm-classloader-test.txt
+# remove the tmp file
+rm /tmp/storm-classloader-test.txt

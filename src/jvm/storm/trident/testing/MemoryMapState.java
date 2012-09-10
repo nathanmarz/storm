12288578be9b0b4ebe5be817312dc5593572216f
Jason Jackson <jackson@twitter.com>
2012-09-10 14:29:47 -0700

    added unit testing for ITupleCollection::getTuples

--- src/jvm/storm/trident/testing/MemoryMapState.java
+++ src/jvm/storm/trident/testing/MemoryMapState.java
@@ -5,40 +5,52 @@ import backtype.storm.tuple.Values;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import storm.trident.state.OpaqueValue;
 import storm.trident.state.State;
 import storm.trident.state.StateFactory;
-import storm.trident.state.map.CachedMap;
-import storm.trident.state.map.IBackingMap;
-import storm.trident.state.map.OpaqueMap;
-import storm.trident.state.map.SnapshottableMap;
-
+import storm.trident.state.ValueUpdater;
+import storm.trident.state.map.*;
+import storm.trident.state.snapshot.Snapshottable;
 
 public class MemoryMapState<T> implements IBackingMap<T>, ITupleCollection {
+
     public static class Factory implements StateFactory {
+
         String _id;
-        
+
         public Factory() {
             _id = UUID.randomUUID().toString();
         }
-        
+
+        static class SnapshottableMapAndTupleCollection<G> extends SnapshottableMap<G> implements ITupleCollection {
+            MemoryMapState _memoryMapState;
+            public SnapshottableMapAndTupleCollection(String id) {
+                super(null, new Values("$MEMORY-MAP-STATE-GLOBAL$"));
+                _memoryMapState = new MemoryMapState(id);
+                setDelegate(OpaqueMap.build(_memoryMapState));
+            }
+
+            public Iterator<List<Object>> getTuples() {
+                return _memoryMapState.getTuples();
+            }
+        }
+
         @Override
         public State makeState(Map conf, int partitionIndex, int numPartitions) {
-            return new SnapshottableMap(OpaqueMap.build(new CachedMap(new MemoryMapState(_id), 10)), new Values("$MEMORY-MAP-STATE-GLOBAL$"));
-        }        
+            return new SnapshottableMapAndTupleCollection(_id);
+        }
     }
-    
+
     public static void clearAll() {
         _dbs.clear();
     }
-    
-    static ConcurrentHashMap<String,  Map<List<Object>, Object>> _dbs = new ConcurrentHashMap<String, Map<List<Object>, Object>>();
-    
+    static ConcurrentHashMap<String, Map<List<Object>, Object>> _dbs = new ConcurrentHashMap<String, Map<List<Object>, Object>>();
     Map<List<Object>, T> db;
     Long currTx;
-    
+
     public MemoryMapState(String id) {
-        if(!_dbs.containsKey(id)) {
-           _dbs.put(id, new HashMap()); 
+        if (!_dbs.containsKey(id)) {
+            _dbs.put(id, new HashMap());
         }
         this.db = (Map<List<Object>, T>) _dbs.get(id);
     }
@@ -46,7 +58,7 @@ public class MemoryMapState<T> implements IBackingMap<T>, ITupleCollection {
     @Override
     public List<T> multiGet(List<List<Object>> keys) {
         List<T> ret = new ArrayList();
-        for(List<Object> key: keys) {
+        for (List<Object> key : keys) {
             ret.add(db.get(key));
         }
         return ret;
@@ -54,27 +66,28 @@ public class MemoryMapState<T> implements IBackingMap<T>, ITupleCollection {
 
     @Override
     public void multiPut(List<List<Object>> keys, List<T> vals) {
-        for(int i=0; i<keys.size(); i++) {
+        for (int i = 0; i < keys.size(); i++) {
             List<Object> key = keys.get(i);
             T val = vals.get(i);
             db.put(key, val);
         }
-    }    
+    }
 
     @Override
     public Iterator<List<Object>> getTuples() {
         return new Iterator<List<Object>>() {
-            private Iterator<Map.Entry<List<Object>,T>> it = db.entrySet().iterator();
+
+            private Iterator<Map.Entry<List<Object>, T>> it = db.entrySet().iterator();
 
             public boolean hasNext() {
                 return it.hasNext();
             }
 
             public List<Object> next() {
-                Map.Entry<List<Object>,T> e = it.next();
-                List<Object> ret = new ArrayList<Object>(e.getKey().size()+1);
+                Map.Entry<List<Object>, T> e = it.next();
+                List<Object> ret = new ArrayList<Object>();
                 ret.addAll(e.getKey());
-                ret.add(e.getValue());
+                ret.add(((OpaqueValue)e.getValue()).getCurr());
                 return ret;
             }
 

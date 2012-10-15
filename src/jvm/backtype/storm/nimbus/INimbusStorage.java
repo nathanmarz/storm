382dfb67727be5aa8cf431b555cdd4be09d6683b
Sergey Lukjanov <me@frostman.ru>
2012-10-15 21:00:23 +0400

    INimbusStorage interface added

---
+++ src/jvm/backtype/storm/nimbus/INimbusStorage.java
@@ -0,0 +1,32 @@
+package backtype.storm.nimbus;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author slukjanov
+ */
+public interface INimbusStorage {
+
+    void init(Map conf);
+
+    InputStream open(String path);
+
+    OutputStream create(String path);
+
+    List<String> list(String path);
+
+    void delete(String path);
+
+    void delete(List<String> path);
+
+    void mkdirs(String path);
+
+    void move(String from, String to);
+
+    boolean isSupportDistributed();
+
+}
+

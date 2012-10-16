25809bb26789c215b538e52cc831a7d3324fcb40
Sergey Lukjanov <me@frostman.ru>
2012-10-16 23:53:40 +0400

    some helpers for working with nimbus storage has been added

--- src/clj/backtype/storm/nimbus/storage.clj
+++ src/clj/backtype/storm/nimbus/storage.clj
@@ -1,8 +1,9 @@
 (ns backtype.storm.nimbus.storage
   (:import [java.io InputStream OutputStream FileInputStream FileOutputStream File])
   (:import [java.util List Map])
-  (:import [org.apache.commons.io FileUtils])
+  (:import [org.apache.commons.io FileUtils IOUtils])
   (:use [backtype.storm log config util])
+  (:import [backtype.storm.utils Utils])
   (:import [backtype.storm.nimbus INimbusStorage]))
 
 (defn create-local-storage [conf]
@@ -48,3 +49,19 @@
     (create-custom-storage storage-name conf)
     (create-local-storage conf)))
 
+(defn upload-file-to-storage [file storage path]
+  (let [stream (.create storage path)]
+    (try
+      (IOUtils/copy (FileInputStream. file) stream)
+      (finally (.close stream)))))
+
+(defn ensure-clean-dir-in-storage [storage path]
+  (.mkdirs storage path)
+  (if-let [files (.list storage path)]
+    (.delete storage files)))
+
+(defn serialize-to-storage [obj storage path]
+  (let [stream (.create storage path)]
+    (try
+      (IOUtils/write (Utils/serialize obj) stream)
+      (finally (.close stream)))))

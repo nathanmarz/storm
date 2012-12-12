bde6ab22f9552515f4e225f9fa9b2a274d518dcd
Nathan Marz <nathan@twitter.com>
2012-12-11 16:07:41 -0800

    fix project.clj

--- project.clj
+++ project.clj
@@ -11,12 +11,10 @@
   :dependencies [
 ;;                 [org.twitter4j/twitter4j-core "2.2.6-SNAPSHOT"]
 ;;                 [org.twitter4j/twitter4j-stream "2.2.6-SNAPSHOT"]
-                   [storm/trident-memcached "0.0.4-wip1"]
                    [commons-collections/commons-collections "3.2.1"]
                  ]
 
   :dev-dependencies [[storm "0.8.2-wip18"]
                      [org.clojure/clojure "1.4.0"]
-;                     [com.thimbleware.jmemcached/jmemcached-cli "1.0.0"]
                      ])
 

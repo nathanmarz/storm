df380fb0186aa59e4d1f2f74e8f609c3bda23dc2
Jason Jackson <jackson@twitter.com>
2012-12-17 22:41:07 -0800

    version bump 0.9.0-wip5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-kafka "0.9.0-wip5-scala292-multischeme"
+(defproject storm/storm-kafka "0.9.0-wip5-scala292"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}

b776b1e9ab812901f1385547095cdecfb7bf496a
Kang Xiao <kxiao.tiger@gmail.com>
2013-01-08 00:16:24 +0800

    remove timeout drpc request from its function's request queue.

--- src/clj/backtype/storm/daemon/drpc.clj
+++ src/clj/backtype/storm/daemon/drpc.clj
@@ -32,10 +32,14 @@
         id->sem (atom {})
         id->result (atom {})
         id->start (atom {})
+        id->function (atom {})
+        id->request (atom {})
         request-queues (atom {})
         cleanup (fn [id] (swap! id->sem dissoc id)
                          (swap! id->result dissoc id)
-                         (swap! id->start dissoc id))
+                         (swap! id->start dissoc id)
+                         (swap! id->function dissoc id)
+                         (swap! id->request dissoc id))
         my-ip (.getHostAddress (InetAddress/getLocalHost))
         clear-thread (async-loop
                       (fn []
@@ -43,6 +47,8 @@
                           (when (> (time-delta start) (conf DRPC-REQUEST-TIMEOUT-SECS))
                             (when-let [sem (@id->sem id)]
                               (swap! id->result assoc id (DRPCExecutionException. "Request timed out"))
+                              (.remove (acquire-queue request-queues (@id->function id)) (@id->request id))
+                              (log-warn "Timeout DRPC request id: " id " start at " start)
                               (.release sem))
                             (cleanup id)
                             ))
@@ -59,6 +65,8 @@
               ]
           (swap! id->start assoc id (current-time-secs))
           (swap! id->sem assoc id sem)
+          (swap! id->function assoc id function)
+          (swap! id->request assoc id req)
           (.add queue req)
           (log-debug "Waiting for DRPC result for " function " " args " at " (System/currentTimeMillis))
           (.acquire sem)

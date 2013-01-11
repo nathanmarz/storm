c40f0ce165a421d2f88f3ce28ee0615dbf19ecad
Jason Jackson <jackson@twitter.com>
2013-01-10 17:01:40 -0800

    bumped version, upgraded storm dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm/storm-kafka "0.9.0-wip5-scala292"
+(defproject storm/storm-kafka "0.9.0-wip15-scala292"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
@@ -6,7 +6,7 @@
   :dependencies [[com.twitter/kafka_2.9.2 "0.7.0"
                   :exclusions [org.apache.zookeeper/zookeeper
                                log4j/log4j]]]
-  :dev-dependencies [[storm "0.9.0-wip9"]
+  :dev-dependencies [[storm "0.9.0-wip15"]
                      [org.slf4j/log4j-over-slf4j "1.6.6"]
                      [ch.qos.logback/logback-classic "1.0.6"]
                      [org.clojure/clojure "1.4.0"]]

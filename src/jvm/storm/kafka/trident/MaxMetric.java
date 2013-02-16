47ce0ae97830380eb85db7ae97a7f7f3adbff820
Jason Jackson <jackson@twitter.com>
2013-02-16 09:40:03 -0800

    fixed NPE in MaxMetric. bumped version 0.9.0-wip16a-scala292

--- src/jvm/storm/kafka/trident/MaxMetric.java
+++ src/jvm/storm/kafka/trident/MaxMetric.java
@@ -12,6 +12,8 @@ public class MaxMetric implements ICombiner<Long> {
 
     @Override
     public Long combine(Long l1, Long l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
         return Math.max(l1, l2);
     }
 

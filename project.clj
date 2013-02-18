8f8f48c75989ea1af3afeb8c0fe27b51b6080850
Nathan Marz <nathan@nathanmarz.com>
2013-02-17 23:59:37 -0800

    upgrade to lein 2

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject storm-starter "0.0.1-SNAPSHOT"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :javac-options {:debug "true" :fork "true"}
-  :resources-path "multilang"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
+  :resource-paths ["multilang"]
   :aot :all
   :repositories {
 ;;                 "twitter4j" "http://twitter4j.org/maven2"
@@ -14,7 +13,8 @@
                    [commons-collections/commons-collections "3.2.1"]
                  ]
 
-  :dev-dependencies [[storm "0.8.2"]
-                     [org.clojure/clojure "1.4.0"]
-                     ])
-
+  :profiles {:dev
+              {:dependencies [[storm "0.8.2"]
+                              [org.clojure/clojure "1.4.0"]]}}
+  :min-lein-version "2.0.0"
+  )

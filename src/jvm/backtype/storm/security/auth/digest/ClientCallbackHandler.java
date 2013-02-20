a22cb5d17d85c7da3035fca835d46114e88aed51
afeng <afeng@yahoo-inc.com>
2013-02-20 11:55:54 -0800

    format cleanup

--- src/jvm/backtype/storm/security/auth/digest/ClientCallbackHandler.java
+++ src/jvm/backtype/storm/security/auth/digest/ClientCallbackHandler.java
@@ -28,7 +28,7 @@ public class ClientCallbackHandler implements CallbackHandler {
     /**
      * Constructor based on a JAAS configuration
      * 
-     * For digest, you should have a pair of user name and password defined in this figgure.
+     * For digest, you should have a pair of user name and password defined.
      * 
      * @param configuration
      * @throws IOException
@@ -43,6 +43,7 @@ public class ClientCallbackHandler implements CallbackHandler {
             throw new IOException(errorMessage);
         }
 
+        _password = "";
         for(AppConfigurationEntry entry: configurationEntries) {
             if (entry.getOptions().get(USERNAME) != null) {
                 _username = (String)entry.getOptions().get(USERNAME);
@@ -68,22 +69,7 @@ public class ClientCallbackHandler implements CallbackHandler {
                 PasswordCallback pc = (PasswordCallback)c;
                 if (_password != null) {
                     pc.setPassword(_password.toCharArray());
-                } else {
-                    LOG.warn("Could not login: the client is being asked for a password, but the " +
-                            " client code does not currently support obtaining a password from the user." +
-                            " Make sure that the client is configured to use a ticket cache (using" +
-                            " the JAAS configuration setting 'useTicketCache=true)' and restart the client. If" +
-                            " you still get this message after that, the TGT in the ticket cache has expired and must" +
-                            " be manually refreshed. To do so, first determine if you are using a password or a" +
-                            " keytab. If the former, run kinit in a Unix shell in the environment of the user who" +
-                            " is running this client using the command" +
-                            " 'kinit <princ>' (where <princ> is the name of the client's Kerberos principal)." +
-                            " If the latter, do" +
-                            " 'kinit -k -t <keytab> <princ>' (where <princ> is the name of the Kerberos principal, and" +
-                            " <keytab> is the location of the keytab file). After manually refreshing your cache," +
-                            " restart this client. If you continue to see this message after manually refreshing" +
-                            " your cache, ensure that your KDC host's clock is in sync with this host's clock.");
-                }
+                } 
             } else if (c instanceof AuthorizeCallback) {
                 LOG.debug("authorization callback");
                 AuthorizeCallback ac = (AuthorizeCallback) c;

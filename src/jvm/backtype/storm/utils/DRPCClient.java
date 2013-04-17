0d2df61f7d13060629dfe6c8706efdfec5b7d371
afeng <afeng@yahoo-inc.com>
2013-04-16 23:56:13 -0700

    clean up DRPCClient.java

--- src/jvm/backtype/storm/utils/DRPCClient.java
+++ src/jvm/backtype/storm/utils/DRPCClient.java
@@ -1,9 +1,9 @@
 package backtype.storm.utils;
 
-import backtype.storm.Config;
 import backtype.storm.generated.DRPCExecutionException;
 import backtype.storm.generated.DistributedRPC;
 import backtype.storm.generated.AuthorizationException;
+
 import org.apache.thrift7.TException;
 import org.apache.thrift7.transport.TTransport;
 import backtype.storm.security.auth.ThriftClient;
@@ -12,11 +12,7 @@ import org.apache.thrift7.transport.TTransportException;
 import java.util.Map;
 
 public class DRPCClient extends ThriftClient implements DistributedRPC.Iface {
-    private TTransport conn;
     private DistributedRPC.Client client;
-    private String host;
-    private int port;
-    private Integer timeout;
 
     public DRPCClient(Map conf, String host, int port) throws TTransportException {
         this(conf, host, port, null);
@@ -24,21 +20,35 @@ public class DRPCClient extends ThriftClient implements DistributedRPC.Iface {
 
     public DRPCClient(Map conf, String host, int port, Integer timeout) throws TTransportException {
         super(conf, host, port, timeout);
-        this.host = host;
-        this.port = port;
         this.client = new DistributedRPC.Client(_protocol);
     }
         
     public String getHost() {
-        return host;
+        return _host;
     }
     
     public int getPort() {
-        return port;
+        return _port;
     }   
     
     public String execute(String func, String args) throws TException, DRPCExecutionException, AuthorizationException {
-        return client.execute(func, args);
+        try {
+            if(client==null)  {
+                connect();
+                if (_protocol != null)
+                    client = new DistributedRPC.Client(_protocol);
+            }
+            return client.execute(func, args);
+        } catch(TException e) {
+            client = null;
+            throw e;
+        } catch(DRPCExecutionException e) {
+            client = null;
+            throw e;
+        } catch(AuthorizationException e) {
+            client = null;
+            throw e;
+        }
     }
 
     public DistributedRPC.Client getClient() {

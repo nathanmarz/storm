9f22f7e566a4135989b4b81cf793ad26a622f49e
anfeng <andy.feng@gmail.com>
2013-05-01 20:39:00 -0700

    move storm-netty into its own module

---
+++ storm-netty/project.clj
@@ -0,0 +1,14 @@
+(def ROOT-DIR (subs *file* 0 (- (count *file*) (count "project.clj"))))
+(def VERSION (-> ROOT-DIR (str "/../VERSION") slurp))
+
+(defproject storm/storm-netty VERSION
+  :dependencies [[storm/storm-core "0.9.0-wip17"]
+  		 [io.netty/netty "3.6.3.Final"]
+                 ]
+
+  :java-source-paths ["src/jvm"]
+  :test-paths ["test/clj"]
+
+  :profiles {:release {}}
+
+  :aot :all)

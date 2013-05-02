e29609532ff7152bfbd4e9d7e44b95e74272560a
anfeng <andy.feng@gmail.com>
2013-05-01 21:51:53 -0700

    remove netty dependency from storm-core/project.clj

--- storm-core/project.clj
+++ storm-core/project.clj
@@ -27,7 +27,6 @@
                  [com.google.guava/guava "13.0"]
                  [ch.qos.logback/logback-classic "1.0.6"]
                  [org.slf4j/log4j-over-slf4j "1.6.6"]
-                 [io.netty/netty "3.6.3.Final"]
                  ]
 
   :source-paths ["src/clj"]

0d02b6554430976b701cf34edca3026d3aedbefd
anfeng <andy.feng@gmail.com>
2013-05-05 09:19:06 -0700

    project.clj fixed for module build

--- storm-netty/project.clj
+++ storm-netty/project.clj
@@ -1,13 +1,17 @@
 (def ROOT-DIR (subs *file* 0 (- (count *file*) (count "project.clj"))))
 (def VERSION (-> ROOT-DIR (str "/../VERSION") slurp))
 
-(defproject storm/storm-netty VERSION
-  :dependencies [[storm/storm-core "0.9.0-wip17"]
+(eval `(defproject storm/storm-netty ~VERSION
+  :url "http://storm-project.net"
+  :description "Distributed and fault-tolerant realtime computation"
+  :license {:name "Eclipse Public License - Version 1.0" :url "https://github.com/nathanmarz/storm/blob/master/LICENSE.html"}
+  :mailing-list {:name "Storm user mailing list"
+                 :archive "https://groups.google.com/group/storm-user"
+                 :post "storm-user@googlegroups.com"}
+  :dependencies [[storm/storm-core ~VERSION]
                  [io.netty/netty "3.6.3.Final"]]
-  
   :source-paths ["src/jvm"]
   :java-source-paths ["src/jvm"]
   :test-paths ["test/clj"]
-  
   :profiles {:release {}}
-  :aot :all)
+  :aot :all))

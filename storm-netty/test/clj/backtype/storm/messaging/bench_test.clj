6866319e7c22a41ed07b5eddbc498be41428fe85
anfeng <andy.feng@gmail.com>
2013-05-11 20:18:47 -0700

    bench test code reorganized

--- storm-netty/test/clj/backtype/storm/messaging/bench_test.clj
+++ storm-netty/test/clj/backtype/storm/messaging/bench_test.clj
@@ -9,41 +9,32 @@
 (def task 1) 
 (def msg_count 100000)
 
+(defn batch-bench [context]
+  (let [server (.bind context nil port)
+        client (.connect context nil "localhost" port)]
+    (doseq [num  (range 1 msg_count)]
+      (let [req_msg (str num)]
+        (.send client task (.getBytes req_msg))))
+    (doseq [num  (range 1 msg_count)]
+      (let [req_msg (str num)
+            resp (.recv server 0)
+            resp_msg (String. (.message resp))]
+        (is (= req_msg resp_msg))))
+    (.close client)
+    (.close server)
+    (.term context)))
+
 (deftest test-netty-perf
   (time (let [storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
                     STORM-MESSAGING-NETTY-BUFFER-SIZE 1024000
                     STORM-MESSAGING-NETTY-MAX-RETRIES 10
                     STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000 
                     STORM-MESSAGING-NETTY-MAX-SLEEP-MS 5000}
-        context (TransportFactory/makeContext storm-conf)
-        server (.bind context nil port)
-        client (.connect context nil "localhost" port)]
-    (doseq [num  (range 1 msg_count)]
-      (let [req_msg (str num)]
-        (.send client task (.getBytes req_msg))))
-    (doseq [num  (range 1 msg_count)]
-      (let [req_msg (str num)
-            resp (.recv server 0)
-            resp_msg (String. (.message resp))]
-        (is (= req_msg resp_msg))))
-    (.close client)
-    (.close server)
-    (.term context))))
+              context (TransportFactory/makeContext storm-conf)]
+          (batch-bench context))))
 
 (deftest test-zmq-perf
   (time (let [storm-conf (merge (read-storm-config)
                           {STORM-MESSAGING-TRANSPORT  "backtype.storm.messaging.zmq"})
-        context (TransportFactory/makeContext storm-conf)
-        server (.bind context nil port)
-        client (.connect context nil "localhost" port)]
-    (doseq [num  (range 1 msg_count)]
-      (let [req_msg (str num)]
-        (.send client task (.getBytes req_msg))))
-    (doseq [num  (range 1 msg_count)]
-      (let [req_msg (str num)
-            resp (.recv server 0)
-            resp_msg (String. (.message resp))]
-        (is (= req_msg resp_msg))))
-    (.close client)
-    (.close server)
-    (.term context))))
+        context (TransportFactory/makeContext storm-conf)]
+          (batch-bench context))))

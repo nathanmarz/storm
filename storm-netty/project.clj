b87b363cc1d92a9f6ad1dda1db49502cb963bcbd
Nathan Marz <nathan@nathanmarz.com>
2013-05-28 14:09:53 -0700

    ensure netty test uses netty transport, slim down project.clj, bump version

--- storm-netty/project.clj
+++ storm-netty/project.clj
@@ -2,15 +2,8 @@
 (def VERSION (-> ROOT-DIR (str "/../VERSION") slurp))
 
 (eval `(defproject storm/storm-netty ~VERSION
-  :url "http://storm-project.net"
-  :description "Distributed and fault-tolerant realtime computation"
-  :license {:name "Eclipse Public License - Version 1.0" :url "https://github.com/nathanmarz/storm/blob/master/LICENSE.html"}
-  :mailing-list {:name "Storm user mailing list"
-                 :archive "https://groups.google.com/group/storm-user"
-                 :post "storm-user@googlegroups.com"}
   :dependencies [[storm/storm-core ~VERSION]
                  [io.netty/netty "3.6.3.Final"]]
-  :source-paths ["src/jvm"]
   :java-source-paths ["src/jvm"]
   :test-paths ["test/clj"]
   :profiles {:release {}}

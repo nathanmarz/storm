319d86e59c817e2b83c562d4b0a4b510522e10f2
Philip (flip) Kromer <flip@infochimps.org>
2013-06-25 00:11:18 +0000

    Fixes to build_release.sh script * storm-console-processor was letting clojure-1.2 meet its dependency list, making for two clojure jars in the tarball. Added a dependency to its project.clj * storm-netty had a dependency itself on storm-core, so the storm-core jar was in both _release and _release/lib. removing the dupe jar from lib/ * build_releases should fail if the build_modules script fails. Made build_modules run set -e, and made build_releases fail if it does * maven is putting the storm-* jar files in target/release+provided, not target/provided. Fixed path. * made pretty banners, made paths be properly quoted * bin/ directory files copy with permissions intact. Also now remove build_release.sh and build_modules.sh, since they won't work * rather than remove _release and *.zip, only removes the specific subdirectory and .zip file. If you run with STORM_KEEP_RELEASE=true, it will not delete the release dir at the end of script (only at beginning) * bin/storm tells you how to roll your own release if you try to launch from a git checkout

--- storm-console-logging/project.clj
+++ storm-console-logging/project.clj
@@ -2,6 +2,8 @@
 (def VERSION (-> ROOT-DIR (str "/../VERSION") slurp (.trim)))
 
 (defproject storm/storm-console-logging VERSION
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                ]
   :resource-paths ["logback"]
 
   :profiles {:release {}

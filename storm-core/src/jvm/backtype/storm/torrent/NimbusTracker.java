17f1acc3d03e99a1c4c7d96d9921211fb8115373
P. Taylor Goetz <ptgoetz@gmail.com>
2013-07-24 13:54:35 -0400

    implement configurable bitorrent rate limits and seeding for nimbus/supervisor

--- storm-core/src/jvm/backtype/storm/torrent/NimbusTracker.java
+++ storm-core/src/jvm/backtype/storm/torrent/NimbusTracker.java
@@ -8,6 +8,7 @@ import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,47 +23,20 @@ import com.turn.ttorrent.common.Torrent;
 import com.turn.ttorrent.tracker.TrackedTorrent;
 import com.turn.ttorrent.tracker.Tracker;
 
-public class NimbusTracker {
+public class NimbusTracker extends BaseTracker {
     private static final Logger LOG = LoggerFactory.getLogger(NimbusTracker.class);
     private Tracker tracker;
     private InetAddress nimbusHost;
-    private HashMap<String, Client> clients = new HashMap<String, Client>();
     private String hostName;
     private Integer port;
-    
-    public void stop(String topologyId){
-        LOG.info("Stop seeding/tracking for topology {}", topologyId);
-        Client client = this.clients.remove(topologyId);
-        if(client != null){
-            Torrent torrent = client.getTorrent();
-            client.stop();
-            this.tracker.remove(torrent);
-        }
-    }
-    
-    public void trackAndSeed(String fileName, String topologyId) throws IOException, NoSuchAlgorithmException, InterruptedException, URISyntaxException{
-        File jarFile = new File(fileName);
-        File destDir = jarFile.getParentFile();
-        LOG.info("Generating torrent for file: {}", jarFile.getAbsolutePath());
-        
-        URI uri = URI.create("http://" + this.hostName + ":" + this.port + "/announce");
-        LOG.info("Creating torrent with annound URL: {}", uri);
-        Torrent torrent = Torrent.create(jarFile, uri, "storm-nimbus");
-        File torrentFile = new File(destDir, "stormjar.torrent");
-        torrent.save(new FileOutputStream(torrentFile));
-        LOG.info("Saved torrent: {}" + torrentFile.getAbsolutePath());
-        this.tracker.announce(new TrackedTorrent(torrent));
-        LOG.info("Torrent announced to tracker.");
-        Client client = new Client(this.nimbusHost, new SharedTorrent(torrent, destDir, true));
-        client.share();
-        this.clients.put(topologyId, client);
-        LOG.info("Seeding torrent...");
-    }
-    
     
     public NimbusTracker (Map conf) throws IOException{
         this.hostName = (String)conf.get(Config.NIMBUS_HOST);
         this.port = (Integer)conf.get(Config.NIMBUS_BITTORRENT_PORT);
+        this.maxDownload = (Double)conf.get(Config.NIMBUS_BITTORRENT_MAX_DOWNLOAD_RATE);
+        this.maxUpload = (Double)conf.get(Config.NIMBUS_BITTORRENT_MAX_UPLOAD_RATE);
+        LOG.info("Download rates [U/D]: {}/{} kB/sec", format(this.maxDownload), format(this.maxDownload));
+        
         String bindAddress = (String)conf.get(Config.NIMBUS_BITTORRENT_BIND_ADDRESS);
         
         LOG.info("Starting bt tracker bound to interface '{}'", bindAddress);
@@ -72,5 +46,43 @@ public class NimbusTracker {
         LOG.info("Announce URL: {}", this.tracker.getAnnounceUrl());
         this.tracker.start();
     }
+    
+    public void stop(String topologyId){
+        LOG.info("Stop seeding/tracking for topology {}", topologyId);
+        Client client = this.clients.remove(topologyId);
+        if(client != null){
+            Torrent torrent = client.getTorrent();
+            client.stop();
+            this.tracker.remove(torrent);
+        }
+        rebalanceRates();
+    }
+    
+    public void trackAndSeed(String dir, String topologyId) throws IOException, NoSuchAlgorithmException, InterruptedException, URISyntaxException{
+        
+        File destDir = new File(dir);
+        LOG.info("Generating torrent for directory: {}", destDir.getAbsolutePath());
+        
+        URI uri = URI.create("http://" + this.hostName + ":" + this.port + "/announce");
+        LOG.info("Creating torrent with announce URL: {}", uri);
+        ArrayList<File> files = new ArrayList<File>();
+        
+        files.add(new File(destDir, "stormjar.jar"));
+        files.add(new File(destDir, "stormconf.ser"));
+        files.add(new File(destDir, "stormcode.ser"));
+        
+        Torrent torrent = Torrent.create(destDir, files, uri, "storm-nimbus");
+        File torrentFile = new File(destDir, topologyId + ".torrent");
+        torrent.save(new FileOutputStream(torrentFile));
+        LOG.info("Saved torrent: {}" + torrentFile.getAbsolutePath());
+        this.tracker.announce(new TrackedTorrent(torrent));
+        LOG.info("Torrent announced to tracker.");
+        Client client = new Client(this.nimbusHost, new SharedTorrent(torrent, destDir.getParentFile(), true));
+        this.clients.put(topologyId, client);
+        rebalanceRates();
+        client.share();
+        LOG.info("Seeding torrent...");
+    }
+
 
 }

3b2db7cfa11d9daf6e326202f3909d9d104f95dd
Michael G. Noll <mnoll@verisign.com>
2013-08-29 12:37:36 +0200

    project.clj: add test dependencies of the Java code in storm-starter

--- project.clj
+++ project.clj
@@ -15,6 +15,10 @@
 
   :profiles {:dev
               {:dependencies [[storm "0.8.2"]
-                              [org.clojure/clojure "1.4.0"]]}}
+                              [org.clojure/clojure "1.4.0"]
+                              [org.testng/testng "6.8.5"]
+                              [org.easytesting/fest-assert-core "2.0M8"]
+                              [org.mockito/mockito-all "1.9.0"]
+                              [org.jmock/jmock "2.6.0"]]}}
   :min-lein-version "2.0.0"
   )

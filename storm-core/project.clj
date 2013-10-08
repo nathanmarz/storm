ff79d6f857faf1002e0577295dd2c73e8ce2a482
P. Taylor Goetz <ptgoetz@gmail.com>
2013-10-08 10:51:36 -0400

    exclude log4j dependencies from ttorrent dependency

--- storm-core/project.clj
+++ storm-core/project.clj
@@ -27,7 +27,8 @@
                  [com.google.guava/guava "13.0"]
                  [ch.qos.logback/logback-classic "1.0.6"]
                  [org.slf4j/log4j-over-slf4j "1.6.6"]
-                 [com.turn/ttorrent "1.2"]
+                 [com.turn/ttorrent "1.2"
+                  :exclusions[org.slf4j/slf4j-log4j12]]
                  ]
 
   :source-paths ["src/clj"]

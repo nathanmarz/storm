b846ea13b75e1ac0ee2aa9289b5c6d88b9a55b3a
Kang Xiao <kxiao.tiger@gmail.com>
2013-11-14 17:20:45 +0800

    implement deep-hash-code in clojure

--- storm-core/src/clj/backtype/storm/tuple.clj
+++ storm-core/src/clj/backtype/storm/tuple.clj
@@ -4,5 +4,33 @@
 
 (bootstrap)
 
+(def ARRAY-TYPES-MAP 
+  {
+    (type (to-array [])) nil
+    (type (byte-array 0)) nil
+    (type (char-array 0)) nil
+    (type (short-array 0)) nil
+    (type (int-array 0)) nil
+    (type (long-array 0)) nil
+    (type (float-array 0)) nil
+    (type (double-array 0)) nil
+    (type (boolean-array 0)) nil
+  })
+
+(defn deep-hash-code [alist]
+  "deep hash code based on array/list content.
+   it's the same as java.util.Arrays.deepHashCode(),
+   but without convert a list to array to avoid copy."
+  (reduce
+    #(.intValue (+ (.intValue (* 31 %1)) (hash-code %2)))
+    1 alist))
+
+(defn hash-code [obj]
+  (let [t (type obj)]
+    (if (contains? ARRAY-TYPES-MAP t)
+      (deep-hash-code obj)
+      (.hashCode obj))))
+
 (defn list-hash-code [^List alist]
-  (java.util.Arrays/deepHashCode (.toArray alist)))
+  (deep-hash-code alist))
+

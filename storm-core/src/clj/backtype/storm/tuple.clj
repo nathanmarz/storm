92e30befcd9be3abe28716b473df222e07f95841
Kang Xiao <kxiao.tiger@gmail.com>
2013-11-16 18:56:53 +0800

    fix according to review comments

--- storm-core/src/clj/backtype/storm/tuple.clj
+++ storm-core/src/clj/backtype/storm/tuple.clj
@@ -4,30 +4,34 @@
 
 (bootstrap)
 
-(def ARRAY-TYPES-MAP 
-  {
-    (type (to-array [])) nil
-    (type (byte-array 0)) nil
-    (type (char-array 0)) nil
-    (type (short-array 0)) nil
-    (type (int-array 0)) nil
-    (type (long-array 0)) nil
-    (type (float-array 0)) nil
-    (type (double-array 0)) nil
-    (type (boolean-array 0)) nil
+(def ARRAY-TYPES
+  #{
+    (type (to-array []))
+    (type (byte-array 0))
+    (type (char-array 0))
+    (type (short-array 0))
+    (type (int-array 0))
+    (type (long-array 0))
+    (type (float-array 0))
+    (type (double-array 0))
+    (type (boolean-array 0))
   })
+
+(declare hash-code)
 
 (defn deep-hash-code [alist]
   "deep hash code based on array/list content.
    it's the same as java.util.Arrays.deepHashCode(),
-   but without convert a list to array to avoid copy."
+   but without convert a list to array to avoid copy.
+
+   use unchecked-* to make sure use int instead of long."
   (reduce
-    #(.intValue (+ (.intValue (* 31 %1)) (hash-code %2)))
+    #(unchecked-add-int (unchecked-multiply-int 31 %1) (hash-code %2))
     1 alist))
 
 (defn hash-code [obj]
   (let [t (type obj)]
-    (if (contains? ARRAY-TYPES-MAP t)
+    (if (contains? ARRAY-TYPES t)
       (deep-hash-code obj)
       (.hashCode obj))))
 

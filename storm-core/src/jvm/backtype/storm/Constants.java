e758480c1dba3c215a37e6444fa47b475b06329b
xiaoqing.yangxq <xiaoqing.yangxq@alibaba-inc.com>
2013-11-19 13:41:36 +0800

    modify SYSTEM_EXECUTOR_ID type for thrift transfer

--- storm-core/src/jvm/backtype/storm/Constants.java
+++ storm-core/src/jvm/backtype/storm/Constants.java
@@ -1,14 +1,16 @@
 package backtype.storm;
 
 import backtype.storm.coordination.CoordinatedBolt;
-import clojure.lang.RT;
+import java.util.ArrayList;
+
+import java.util.Arrays;
 
 
 public class Constants {
     public static final String COORDINATED_STREAM_ID = CoordinatedBolt.class.getName() + "/coord-stream"; 
 
     public static final long SYSTEM_TASK_ID = -1;
-    public static final Object SYSTEM_EXECUTOR_ID = RT.readString("[-1 -1]");
+    public static final ArrayList<Integer> SYSTEM_EXECUTOR_ID = new ArrayList<Integer>(Arrays.asList(-1,-1));
     public static final String SYSTEM_COMPONENT_ID = "__system";
     public static final String SYSTEM_TICK_STREAM_ID = "__tick";
     public static final String METRICS_COMPONENT_ID_PREFIX = "__metrics";

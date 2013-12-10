421c44664a10145252dbe79a7c464e14c502e849
caofangkun <caofangkun@gmail.com>
2013-12-10 12:47:14 +0800

    use "-c " command to define Number config value will generate ClassCastException

--- storm-core/src/jvm/backtype/storm/StormSubmitter.java
+++ storm-core/src/jvm/backtype/storm/StormSubmitter.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.thrift7.TException;
 import org.json.simple.JSONValue;
 
@@ -59,6 +60,12 @@ public class StormSubmitter {
         }
         stormConf = new HashMap(stormConf);
         stormConf.putAll(Utils.readCommandLineOpts());
+	for (Object confName : stormConf.keySet()) {
+		String confValue = String.valueOf(stormConf.get(confName));
+		if (NumberUtils.isNumber(confValue)) {
+			stormConf.put(confName, NumberUtils.createNumber(confValue));
+		}
+	}
         Map conf = Utils.readStormConfig();
         conf.putAll(stormConf);
         try {

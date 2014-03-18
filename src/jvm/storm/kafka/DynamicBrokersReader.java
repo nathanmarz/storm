84f04c3fea11ccb6844bdef7baf8e6f654637981
P. Taylor Goetz <ptgoetz@gmail.com>
2014-03-18 15:36:09 -0400

    align with 0.9.2-incubating dependencies.

--- src/jvm/storm/kafka/DynamicBrokersReader.java
+++ src/jvm/storm/kafka/DynamicBrokersReader.java
@@ -26,17 +26,13 @@ public class DynamicBrokersReader {
     public DynamicBrokersReader(Map conf, String zkStr, String zkPath, String topic) {
         _zkPath = zkPath;
         _topic = topic;
-        try {
-            _curator = CuratorFrameworkFactory.newClient(
-                    zkStr,
-                    Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_SESSION_TIMEOUT)),
-                    15000,
-                    new RetryNTimes(Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_TIMES)),
-                            Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_INTERVAL))));
-            _curator.start();
-        } catch (IOException ex) {
-            LOG.error("can't connect to zookeeper");
-        }
+        _curator = CuratorFrameworkFactory.newClient(
+                zkStr,
+                Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_SESSION_TIMEOUT)),
+                15000,
+                new RetryNTimes(Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_TIMES)),
+                        Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_INTERVAL))));
+        _curator.start();
     }
 
     /**

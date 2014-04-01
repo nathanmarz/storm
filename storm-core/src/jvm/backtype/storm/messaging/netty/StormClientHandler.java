94c4d4d9e6c4ce736141668c585818214a9d26cf
Robert (Bobby) Evans <evans@yahoo-inc.com>
2014-04-01 15:32:31 +0000

    STORM-12 Reduce thread usage of Netty transport.

--- storm-core/src/jvm/backtype/storm/messaging/netty/StormClientHandler.java
+++ storm-core/src/jvm/backtype/storm/messaging/netty/StormClientHandler.java
@@ -17,7 +17,15 @@
  */
 package backtype.storm.messaging.netty;
 
-import org.jboss.netty.channel.*;
+import java.net.ConnectException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,12 +35,10 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class StormClientHandler extends SimpleChannelUpstreamHandler  {
     private static final Logger LOG = LoggerFactory.getLogger(StormClientHandler.class);
     private Client client;
-    private AtomicBoolean being_closed;
-    long start_time; 
+    long start_time;
     
     StormClientHandler(Client client) {
         this.client = client;
-        being_closed = new AtomicBoolean(false);
         start_time = System.currentTimeMillis();
     }
 
@@ -41,13 +47,14 @@ public class StormClientHandler extends SimpleChannelUpstreamHandler  {
         //register the newly established channel
         Channel channel = event.getChannel();
         client.setChannel(channel);
-        LOG.debug("connection established to a remote host");
+        LOG.info("connection established from "+channel.getLocalAddress()+" to "+channel.getRemoteAddress());
         
-        //send next request
+        //send next batch of requests if any
         try {
-            sendRequests(channel, client.takeMessages());
-        } catch (InterruptedException e) {
-            channel.close();
+            client.tryDeliverMessages(false);
+        } catch (Exception ex) {
+            LOG.info("exception when sending messages:", ex.getMessage());
+            client.reconnect();
         }
     }
 
@@ -60,62 +67,21 @@ public class StormClientHandler extends SimpleChannelUpstreamHandler  {
         if (msg==ControlMessage.FAILURE_RESPONSE)
             LOG.info("failure response:{}", msg);
 
-        //send next request
-        Channel channel = event.getChannel();
+        //send next batch of requests if any
         try {
-            sendRequests(channel, client.takeMessages());
-        } catch (InterruptedException e) {
-            channel.close();
+            client.tryDeliverMessages(false);
+        } catch (Exception ex) {
+            LOG.info("exception when sending messages:", ex.getMessage());
+            client.reconnect();
         }
-    }
-
-    /**
-     * Retrieve a request from message queue, and send to server
-     * @param channel
-     */
-    private void sendRequests(Channel channel, final MessageBatch requests) {
-        if (requests==null || requests.size()==0 || being_closed.get()) return;
-
-        //if task==CLOSE_MESSAGE for our last request, the channel is to be closed
-        Object last_msg = requests.get(requests.size()-1);
-        if (last_msg==ControlMessage.CLOSE_MESSAGE) {
-            being_closed.set(true);
-            requests.remove(last_msg);
-        }
-
-        //we may don't need do anything if no requests found
-        if (requests.isEmpty()) {
-            if (being_closed.get())
-                client.close_n_release();
-            return;
-        }
-
-        //write request into socket channel
-        ChannelFuture future = channel.write(requests);
-        future.addListener(new ChannelFutureListener() {
-            public void operationComplete(ChannelFuture future)
-                    throws Exception {
-                if (!future.isSuccess()) {
-                    LOG.info("failed to send requests:", future.getCause());
-                    future.getChannel().close();
-                } else {
-                    LOG.debug("{} request(s) sent", requests.size());
-                }
-                if (being_closed.get())
-                    client.close_n_release();
-            }
-        });
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent event) {
         Throwable cause = event.getCause();
         if (!(cause instanceof ConnectException)) {
-            LOG.info("Connection failed:", cause);
-        } 
-        if (!being_closed.get()) {
-            client.setChannel(null);
-            client.reconnect();
+            LOG.info("Connection to "+client.remote_addr+" failed:", cause);
         }
+        client.reconnect();
     }
 }

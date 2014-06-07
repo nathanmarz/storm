5f7520af012f9e5ab3bf7e051b0410aaf765bd61
Sean Zhong <clockfly@gmail.com>
2014-06-08 01:55:50 +0800

    STORM-297: wait for the bolt to finish before killing the topology, otherwise netty client may be killed message before all bolts finish.

--- storm-core/test/clj/backtype/storm/messaging_test.clj
+++ storm-core/test/clj/backtype/storm/messaging_test.clj
@@ -85,7 +85,7 @@
                                                  :parallelism-hint 4)
                         })
             results (complete-topology cluster
-                                       topology)]
+                                       topology :kill-waiting 10)]
         
         ;; No error Tuple from Bolt TestEventOrderCheckBolt
         (is (empty? (read-tuples results "2"))))))

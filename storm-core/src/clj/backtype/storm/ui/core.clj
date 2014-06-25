e5efcf506856c472ae7e5e36578862d2c8dd4906
Kishor Patil <kpatil@yahoo-inc.com>
2014-06-25 23:49:08 +0000

    Use javascript to color-code recent errors

--- storm-core/src/clj/backtype/storm/ui/core.clj
+++ storm-core/src/clj/backtype/storm/ui/core.clj
@@ -289,11 +289,11 @@
        (map nil-to-zero)
        (apply max)))
 
-(defn is-error-recent? 
+(defn get-error-time
   [error]
-  (if error (< (time-delta (.get_error_time_secs ^ErrorInfo error))
-                    (* 60 30))
-    false))
+  (if error
+    (time-delta (.get_error_time_secs ^ErrorInfo error))
+    ""))
 
 (defn get-error-data 
   [error]
@@ -561,7 +561,7 @@
      "errorHost" error-host
      "errorPort" error-port
      "errorWorkerLogLink" (worker-log-link error-host error-port)
-     (if (is-error-recent? last-error) "isRecent" "isNotRecent") true
+     "errorLapsedSecs" (get-error-time last-error)
      "lastError" (get-error-data last-error) }))
 
 (defn bolt-comp [top-id summ-map errors window include-sys?]
@@ -587,7 +587,7 @@
      "errorHost" error-host
      "errorPort" error-port
      "errorWorkerLogLink" (worker-log-link error-host error-port)
-     (if (is-error-recent? last-error) "isRecent" "isNotRecent") true
+     "errorLapsedSecs" (get-error-time last-error)
      "lastError" (get-error-data last-error) }))
 
 (defn topology-summary [^TopologyInfo summ]
@@ -693,7 +693,7 @@
         "errorHost" (.get_host e)
         "errorPort"  (.get_port e)
         "errorWorkerLogLink"  (worker-log-link (.get_host e) (.get_port e))
-        (if (is-error-recent? e) "isRecent" "isNotRecent") true
+        "errorLapsedSecs" (get-error-time e)
         "error" (.get_error e)})}))
 
 (defn spout-stats

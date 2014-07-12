cc5f1f632a0d4b81ebbf57668ce025a7780b7a0f
Jungtaek Lim <kabhwan@gmail.com>
2014-07-12 09:23:44 +0900

    Remove magic numbers to be easily understood

--- storm-core/test/clj/backtype/storm/config_test.clj
+++ storm-core/test/clj/backtype/storm/config_test.clj
@@ -66,28 +66,33 @@
                 (catch Exception e e)))))))
 
 (deftest test-integer-validator
-   (let [validator ConfigValidation/IntegerValidator]
-        (.validateField validator "test" 1000)
-        (is (thrown-cause? java.lang.IllegalArgumentException
-           (.validateField validator "test" 1.34)))
-        (is (thrown-cause? java.lang.IllegalArgumentException
-           (.validateField validator "test" 9223372036854775807)))))
+  (let [validator ConfigValidation/IntegerValidator]
+    (.validateField validator "test" nil)
+    (.validateField validator "test" 1000)
+    (is (thrown-cause? java.lang.IllegalArgumentException
+          (.validateField validator "test" 1.34)))
+    (is (thrown-cause? java.lang.IllegalArgumentException
+          (.validateField validator "test" (inc Integer/MAX_VALUE))))))
 
 (deftest test-integers-validator
   (let [validator ConfigValidation/IntegersValidator]
+    (.validateField validator "test" nil)
     (.validateField validator "test" [1000 0 -1000])
     (is (thrown-cause? java.lang.IllegalArgumentException
           (.validateField validator "test" [0 10 1.34])))
     (is (thrown-cause? java.lang.IllegalArgumentException
-          (.validateField validator "test" [-100 9223372036854775807])))))
+          (.validateField validator "test" [0 nil])))
+    (is (thrown-cause? java.lang.IllegalArgumentException
+          (.validateField validator "test" [-100 (inc Integer/MAX_VALUE)])))))
 
 (deftest test-double-validator
   (let [validator ConfigValidation/DoubleValidator]
+    (.validateField validator "test" nil)
     (.validateField validator "test" 10)
     ;; we can provide lenient way to convert int/long to double with losing precision
-    (.validateField validator "test" 2147483647)
-    (.validateField validator "test" 9223372036854775807)
-    (.validateField validator "test" 1.7976931348623157E308)))
+    (.validateField validator "test" Integer/MAX_VALUE)
+    (.validateField validator "test" (inc Integer/MAX_VALUE))
+    (.validateField validator "test" Double/MAX_VALUE)))
 
 (deftest test-topology-workers-is-integer
   (let [validator (CONFIG-SCHEMA-MAP TOPOLOGY-WORKERS)]
@@ -99,7 +104,7 @@
   (let [validator (CONFIG-SCHEMA-MAP TOPOLOGY-STATS-SAMPLE-RATE)]
     (.validateField validator "test" 0.5)
     (.validateField validator "test" 10)
-    (.validateField validator "test" 1.7976931348623157E308)))
+    (.validateField validator "test" Double/MAX_VALUE)))
 
 (deftest test-isolation-scheduler-machines-is-map
   (let [validator (CONFIG-SCHEMA-MAP ISOLATION-SCHEDULER-MACHINES)]

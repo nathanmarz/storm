24749f1b18508de151e892dbb756c721bec29f1b
Kishor Patil <kpatil@yahoo-inc.com>
2014-08-28 00:30:23 -0500

    Correct argument order to retry construct and fix unit tests

--- storm-core/src/jvm/backtype/storm/utils/Utils.java
+++ storm-core/src/jvm/backtype/storm/utils/Utils.java
@@ -361,8 +361,8 @@ public class Utils {
                 .sessionTimeoutMs(Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_SESSION_TIMEOUT)))
                 .retryPolicy(new StormBoundedExponentialBackoffRetry(
                             Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_INTERVAL)),
-                            Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_TIMES)),
-                            Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_INTERVAL_CEILING))));
+                            Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_INTERVAL_CEILING)),
+                            Utils.getInt(conf.get(Config.STORM_ZOOKEEPER_RETRY_TIMES))));
         if(auth!=null && auth.scheme!=null) {
             builder = builder.authorization(auth.scheme, auth.payload);
         }

99aeb80b074f1dc48a396b21ec5ca30a8ed09ddb
Sriharsha Chintalapani <mail@harsha.io>
2015-02-24 13:35:12 -0800

    STORM-687. Storm UI does not display up to date information despite refreshes in IE.

--- storm-core/src/clj/backtype/storm/ui/core.clj
+++ storm-core/src/clj/backtype/storm/ui/core.clj
@@ -867,8 +867,9 @@
 (defnk json-response
   [data callback :serialize-fn to-json :status 200]
      {:status status
-      :headers (if (not-nil? callback) {"Content-Type" "application/javascript;charset=utf-8"}
-                {"Content-Type" "application/json;charset=utf-8"})
+      :headers (merge {"Cache-Control" "no-cache, no-store"}
+                      (if (not-nil? callback) {"Content-Type" "application/javascript;charset=utf-8"}
+                          {"Content-Type" "application/json;charset=utf-8"}))
       :body (if (not-nil? callback)
               (wrap-json-in-callback callback (serialize-fn data))
               (serialize-fn data))})

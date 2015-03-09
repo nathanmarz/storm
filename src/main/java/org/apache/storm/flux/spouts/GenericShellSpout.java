6d446667264b26afdcc528535facc92042b0eb83
P. Taylor Goetz <ptgoetz@gmail.com>
2015-03-09 01:30:20 -0400

    allow output field definition

--- src/main/java/org/apache/storm/flux/spouts/GenericShellSpout.java
+++ src/main/java/org/apache/storm/flux/spouts/GenericShellSpout.java
@@ -3,18 +3,22 @@ package org.apache.storm.flux.spouts;
 import backtype.storm.spout.ShellSpout;
 import backtype.storm.topology.IRichSpout;
 import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
 
 import java.util.Map;
 
 public class GenericShellSpout extends ShellSpout implements IRichSpout {
+    private String[] outputFields;
 
-    public GenericShellSpout(String[] args){
-        System.out.println("Creating GenericShellSpout with args: " + args);
+    public GenericShellSpout(String[] args, String[] outputFields){
+        super(args);
+        this.outputFields = outputFields;
+
     }
 
     @Override
-    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields(this.outputFields));
     }
 
     @Override

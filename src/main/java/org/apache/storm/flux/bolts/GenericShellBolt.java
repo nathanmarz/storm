75f909cd48ae8e08381cc7e58829b97bee4142be
P. Taylor Goetz <ptgoetz@gmail.com>
2015-03-09 22:36:31 -0400

    update contructor to incude declared fields.

--- src/main/java/org/apache/storm/flux/bolts/GenericShellBolt.java
+++ src/main/java/org/apache/storm/flux/bolts/GenericShellBolt.java
@@ -3,18 +3,21 @@ package org.apache.storm.flux.bolts;
 import backtype.storm.task.ShellBolt;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
 
 import java.util.Map;
 
 public class GenericShellBolt extends ShellBolt implements IRichBolt{
+    private String[] outputFields;
 
-    public GenericShellBolt(String[] command){
+    public GenericShellBolt(String[] command, String[] outputFields){
         super(command);
+        this.outputFields = outputFields;
     }
 
     @Override
-    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields(this.outputFields));
     }
 
     @Override

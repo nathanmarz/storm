fe273d415b544e620125937145611e8bf07ea998
P. Taylor Goetz <ptgoetz@gmail.com>
2015-03-10 20:56:29 -0400

    add javadoc

--- src/main/java/org/apache/storm/flux/FluxMain.java
+++ src/main/java/org/apache/storm/flux/FluxMain.java
@@ -63,7 +63,6 @@ public class FluxMain {
 
         // create spouts
         for(SpoutDef sd : topologyDef.getSpouts()){
-            // TODO deal with spouts with non-default constructors
             builder.setSpout(sd.getId(), buildSpout(sd), sd.getParallelism());
         }
 
@@ -86,19 +85,50 @@ public class FluxMain {
             // if the streamId is defined, use it for the grouping, otherwise assume storm's default stream
             String streamId = (grouping.getStreamId() == null ? Utils.DEFAULT_STREAM_ID : grouping.getStreamId());
 
-            // TODO make grouping types an enum
-            if(grouping.getType() == GroupingDef.Type.SHUFFLE){
-                declarer.shuffleGrouping(stream.getFrom(), streamId);
-            } else if(grouping.getType() == GroupingDef.Type.FIELDS){
-                declarer.fieldsGrouping(stream.getFrom(), streamId, new Fields(grouping.getArgs()));
-            } else {
-                throw new UnsupportedOperationException("unsupported grouping type: " + grouping.getStreamId());
+
+            switch(grouping.getType()) {
+                case SHUFFLE:
+                    declarer.shuffleGrouping(stream.getFrom(), streamId);
+                    break;
+                case FIELDS:
+                    //TODO check for null grouping args
+                    declarer.fieldsGrouping(stream.getFrom(), streamId, new Fields(grouping.getArgs()));
+                    break;
+                case ALL:
+                    declarer.allGrouping(stream.getFrom(), streamId);
+                    break;
+                case DIRECT:
+                    declarer.directGrouping(stream.getFrom(), streamId);
+                    break;
+                case GLOBAL:
+                    declarer.globalGrouping(stream.getFrom(), streamId);
+                    break;
+                case LOCAL_OR_SHUFFLE:
+                    declarer.localOrShuffleGrouping(stream.getFrom(), streamId);
+                    break;
+                case NONE:
+                    declarer.noneGrouping(stream.getFrom(), streamId);
+                    break;
+                // TODO custom groupings
+                default:
+                    throw new UnsupportedOperationException("unsupported grouping type: " + grouping);
             }
         }
         return builder.createTopology();
     }
 
 
+    /**
+     * Given a spout definition, return a Storm spout implementation by attempting to find a matching constructor
+     * in the given spout class. Perform list to array conversion as necessary.
+     * @param def
+     * @return
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     */
     private static IRichSpout buildSpout(SpoutDef def) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Class clazz = Class.forName(def.getClassName());
         IRichSpout spout = null;
@@ -117,6 +147,18 @@ public class FluxMain {
         return spout;
     }
 
+
+    /**
+     * Given a list of bolt definitions, build a map of Storm bolts with the bolt definition id as the key.
+     * Attempt to coerce the given constructor arguments to a matching bolt constructor as much as possible.
+     * @param boltDefs
+     * @return
+     * @throws ClassNotFoundException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     */
     private static Map<String, Object> buildBoltMap(List<BoltDef> boltDefs) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         Map<String, Object> retval= new HashMap<String, Object>();
         for(BoltDef def : boltDefs){
@@ -141,6 +183,14 @@ public class FluxMain {
         return retval;
     }
 
+    /**
+     * Given a list of constructor arguments, and a target class, attempt to find a suitable constructor.
+     *
+     * @param args
+     * @param target
+     * @return
+     * @throws NoSuchMethodException
+     */
     private static Constructor findCompatibleConstructor(List<Object> args, Class target) throws NoSuchMethodException {
         Constructor retval = null;
         int eligibleCount= 0;
@@ -170,6 +220,15 @@ public class FluxMain {
     }
 
 
+    /**
+     * Given a java.util.List of contructor arguments, and a java.lang.Constructor instance, attempt to convert the
+     * list to an java.lang.Object array that can be used to invoke the constructor. If a constructor argument needs
+     * to be coerced from a List to an Array, do so.
+     *
+     * @param args
+     * @param constructor
+     * @return
+     */
     private static Object[] getConstructorArgsWithListCoercian(List<Object> args, Constructor constructor){
         Class[] parameterTypes = constructor.getParameterTypes();
         if(parameterTypes.length != args.size()) {
@@ -214,6 +273,13 @@ public class FluxMain {
         return constructorParams;
     }
 
+    /**
+     * Determine if the given constructor can be invoked with the given arguments List. Consider if
+     * list coercian can make it possible.
+     * @param args
+     * @param constructor
+     * @return
+     */
     private static boolean canInvokeConstructorWithArgs(List<Object> args, Constructor constructor) {
         Class[] parameterTypes = constructor.getParameterTypes();
         if(parameterTypes.length != args.size()) {

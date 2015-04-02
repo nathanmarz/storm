6602c4becb7f22a14faab032872e51f70864530a
Derek Dagit <derekd@yahoo-inc.com>
2015-04-02 08:55:03 -0500

    Skip ack init when there are no output tasks

--- storm-core/src/clj/backtype/storm/daemon/executor.clj
+++ storm-core/src/clj/backtype/storm/daemon/executor.clj
@@ -526,7 +526,8 @@
                                                                         out-tuple
                                                                         overflow-buffer)
                                                            ))
-                                         (if rooted?
+                                         (if (and rooted?
+                                                  (seq out-ids)) ;; not empty
                                            (do
                                              (.put pending root-id [task-id
                                                                     message-id

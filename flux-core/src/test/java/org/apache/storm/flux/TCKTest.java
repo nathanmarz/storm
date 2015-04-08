4a1db96fc38fb438c3d4433381e719ca16d63bc8
P. Taylor Goetz <ptgoetz@gmail.com>
2015-04-07 23:23:05 -0400

    add HBase example

--- flux-core/src/test/java/org/apache/storm/flux/TCKTest.java
+++ flux-core/src/test/java/org/apache/storm/flux/TCKTest.java
@@ -82,6 +82,16 @@ public class TCKTest {
     }
 
     @Test
+    public void testHbase() throws Exception {
+        TopologyDef topologyDef = FluxParser.parseResource("/configs/simple_hbase.yaml", false, true, null, false);
+        Config conf = FluxBuilder.buildConfig(topologyDef);
+        ExecutionContext context = new ExecutionContext(topologyDef, conf);
+        StormTopology topology = FluxBuilder.buildTopology(context);
+        assertNotNull(topology);
+        topology.validate();
+    }
+
+    @Test
     public void testIncludes() throws Exception {
         TopologyDef topologyDef = FluxParser.parseResource("/configs/include_test.yaml", false, true, null, false);
         Config conf = FluxBuilder.buildConfig(topologyDef);

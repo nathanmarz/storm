7d5285545a4d4f227f7f8616c2f769f055649560
ddragan <dmitro.dragan@gmail.com>
2015-09-25 17:58:07 +0300

    [STORM-123], add check case for external change of 'now' value.

--- external/storm-kafka/src/jvm/storm/kafka/KafkaSpout.java
+++ external/storm-kafka/src/jvm/storm/kafka/KafkaSpout.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -154,8 +154,8 @@ public class KafkaSpout extends BaseRichSpout {
             }
         }
 
-        long now = System.currentTimeMillis();
-        if ((now - _lastUpdateMs) > _spoutConfig.stateUpdateIntervalMs) {
+        long diffWithNow = System.currentTimeMillis() - _lastUpdateMs;
+        if (diffWithNow > _spoutConfig.stateUpdateIntervalMs || diffWithNow < 0) {
             commit();
         }
     }
@@ -185,11 +185,11 @@ public class KafkaSpout extends BaseRichSpout {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
-	if (_spoutConfig.topicAsStreamId) {
-	    declarer.declareStream(_spoutConfig.topic, _spoutConfig.scheme.getOutputFields());
-	} else {
+        if (_spoutConfig.topicAsStreamId) {
+            declarer.declareStream(_spoutConfig.topic, _spoutConfig.scheme.getOutputFields());
+        } else {
             declarer.declare(_spoutConfig.scheme.getOutputFields());
-	}
+        }
     }
 
     private void commit() {

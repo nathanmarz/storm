d58ba0099f0900ffe8b45abe9f5a27e4379fb232
Roshan Naik <roshan@hortonworks.com>
2016-02-04 18:06:07 -0800

    STORM-1526  fix perf issue related to clojure dynamic method lookup in the spout.nextTuple() call tree

--- storm-core/src/clj/org/apache/storm/daemon/executor.clj
+++ storm-core/src/clj/org/apache/storm/daemon/executor.clj
@@ -58,7 +58,7 @@
   (.prepare grouping context (GlobalStreamId. component-id stream-id) target-tasks)
   (if (instance? LoadAwareCustomStreamGrouping grouping)
     (fn [task-id ^List values load]
-      (.chooseTasks grouping task-id values load))
+      (.chooseTasks ^LoadAwareCustomStreamGrouping grouping task-id values load))
     (fn [task-id ^List values load]
       (.chooseTasks grouping task-id values))))
 

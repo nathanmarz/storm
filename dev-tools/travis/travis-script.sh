c054d1d2c038e5e886288f7cbe78751cadf266d9
Robert (Bobby) Evans <evans@yahoo-inc.com>
2016-02-10 15:59:38 +0000

    Fixed missed heap settings

--- dev-tools/travis/travis-script.sh
+++ dev-tools/travis/travis-script.sh
@@ -25,7 +25,7 @@ cd ${STORM_SRC_ROOT_DIR}
 # We should be concerned that Travis CI could be very slow because it uses VM
 export STORM_TEST_TIMEOUT_MS=150000
 # Travis only has 3GB of memory, lets use 1GB for build, and 1.5GB for forked JVMs
-#export MAVEN_OPTS="-Xmx1024m"
+export MAVEN_OPTS="-Xmx1024m"
 
 mvn --batch-mode test -fae -Pnative,all-tests -Prat -pl "$2"
 BUILD_RET_VAL=$?
